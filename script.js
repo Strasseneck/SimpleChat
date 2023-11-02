6da875fc1f9450031908f1fe5c39264dd48a7467
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-11-02 13:40:54 +0100

    Add date and time tag as span

--- script.js
+++ script.js
@@ -30,48 +30,48 @@ const unfriendlyBotResponses = {
 $('#user-message-button').on('click', function() {
     // create user message and display 
     let userInput = $('#user-message-compose').val();
+
+    // get current date and time for tag
+    let now = new Date();
+    let date = now.getDate() + '/' + (now.getMonth() +1) + '/' + now.getFullYear();
+    let time = now.getHours() + ':' + now.getMinutes();
+    let dateTime = date + ' ' + time;
+    let tag = $('<span>').addClass('tag').text(dateTime);
+
     $('<div>')
     .addClass('user-message')
     .text(userInput)
-    .appendTo('#messages');
+    .append(tag)
+    .appendTo('#messages')
 
     // reset the text area to placeholder
     $('#user-message-compose').val('').blur();
 
     // trigger bot repsonse
     setTimeout(() => {
-        botRespondingAnimation();
-    },1000)
-})
-
-function botRespondingAnimation() {
-    // show bot typing animation
-    $('<p>').addClass('bot-replying')
-    .text('...')
-    .appendTo('#messages');
-
-    // trigger actual reply
-    setTimeout(() => {
         botReply();
     },2000)
-}
-
+})
 
 function botReply() {
-     // generate random reply from options
+    console.log()
+    // generate random reply from options
      let replyIndex = Math.floor(Math.random() * 5);
      let randomReply = friendlyBotResponses[replyIndex];
     
-     // remove animation
-     $('#bot-replying').remove();
+    // get current date and time for tag
+    let now = new Date();
+    let date = now.getDate() + '/' + (now.getMonth() +1) + '/' + now.getFullYear();
+    let time = now.getHours() + ':' + now.getMinutes();
+    let dateTime = date + ' ' + time;
+    let tag = $('<span>').addClass('tag').text(dateTime);
 
-     // create bot message and display 
+    // create bot message and display 
     $('<div>')
     .addClass('bot-message')
     .text(randomReply)
-    .appendTo('#messages');
-
-     
+    .append(tag)
+    .appendTo('#messages');    
 }
 
    
