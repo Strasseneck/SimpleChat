d1c0715e0d661220432824af28f95156c4417818
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-11-02 14:34:09 +0100

    Add different bots for replies

--- script.js
+++ script.js
@@ -6,67 +6,125 @@
 
 const friendlyBotResponses = {
 
-    1: 'response one',
-    2: 'response two',
-    3: 'response three',
-    4: 'response four',
-    5: 'response five'
+    1: 'That\'s really nice of you, I feel the same!',
+    2: 'Well I just think you\'re the best!',
+    3: 'I really like talking to you, you\'re so intelligent!',
+    4: 'A great idea! Let\s do it!',
+    5: 'I\'m so happy to hear you say that!'
 };
 
 // Unfriendly bot
 
 const unfriendlyBotResponses = {
 
-    1: 'response one',
-    2: 'response two',
-    3: 'response three',
-    4: 'response four',
-    5: 'response five'
+    1: 'That\'s one of the stupidest things I\'ve ever heard anyone say!',
+    2: 'BORING! BORING!',
+    3: 'Oh no, it\'s you again, don\'t you have any other friends!',
+    4: 'Did you hear the one about the loser who spends all their time talking to a bot?',
+    5: 'How embarrassing! You really are the worst!'
 };
+
+// Annoying kid
+
+const annoyingKidResponses = {
+   
+    1: 'dUh!',
+    2: 'WATeva!!!',
+    3: 'LOLOLOLOLOLZZZZZ!!!!',
+    4: 'stooooPIiiiiiDDDDD!!!',
+    5: 'nanananananana'
+
+}
+
+// Light or Dark
+
+// Which bot?
+
+// Bot initial states, friendly as default
+
+let friendlyBot = true;
+let unfriendlyBot = false;
+let annoyingKid = false;
+
+// Bot selection
+
+// Friendly Bot clicked
+$('#friendly-bot-btn').on('click', function() {
+    friendlyBot = true;
+    unfriendlyBot = false;
+    annoyingKid = false;
+})
+
+// Unfriendly Bot clicked
+$('#unfriendly-bot-btn').on('click', function() {
+    friendlyBot = false;
+    unfriendlyBot = true;
+    annoyingKid = false;
+})
+
+// Annoying Kid Bot clicked
+$('#annoying-kid-bot-btn').on('click', function() {
+    friendlyBot = false;
+    unfriendlyBot = false;
+    annoyingKid = true;
+})
 
 // Messaging
 
 // Send new message on click
 $('#user-message-button').on('click', function() {
-    // create user message and display 
+    // Create user message and display 
     let userInput = $('#user-message-compose').val();
 
-    // get current date and time for tag
+    // Get current date and time for tag
     let now = new Date();
     let date = now.getDate() + '/' + (now.getMonth() +1) + '/' + now.getFullYear();
     let time = now.getHours() + ':' + now.getMinutes();
     let dateTime = date + ' ' + time;
-    let tag = $('<span>').addClass('tag').text(dateTime);
+    let tag = $('<div>').addClass('tag').text(dateTime);
 
+    // Create the message and display
     $('<div>')
     .addClass('user-message')
     .text(userInput)
     .append(tag)
     .appendTo('#messages')
 
-    // reset the text area to placeholder
+    // Reset the text area to placeholder
     $('#user-message-compose').val('').blur();
 
-    // trigger bot repsonse
+    // Trigger bot repsonse
     setTimeout(() => {
-        botReply();
+        botReply(userInput);
     },2000)
 })
 
-function botReply() {
-    console.log()
-    // generate random reply from options
+function botReply(userInput) {
+    // Generate random reply from options
      let replyIndex = Math.floor(Math.random() * 5);
-     let randomReply = friendlyBotResponses[replyIndex];
+     let randomReply = 'Something has gone wrong!';
+
+     if(friendlyBot === true) {
+        // Case friendly bot
+        randomReply = friendlyBotResponses[replyIndex];
+     }
+     else if(unfriendlyBot === true) {
+        // Case unfriendly bot
+        randomReply = unfriendlyBotResponses[replyIndex];
+     }
+     else {
+        // Case annoying child
+        randomReply = userInput + ' ' + annoyingKidResponses[replyIndex];
+     }
     
-    // get current date and time for tag
+    // Get current date and time for tag
     let now = new Date();
     let date = now.getDate() + '/' + (now.getMonth() +1) + '/' + now.getFullYear();
     let time = now.getHours() + ':' + now.getMinutes();
     let dateTime = date + ' ' + time;
-    let tag = $('<span>').addClass('tag').text(dateTime);
+    let tag = $('<div>').addClass('tag').html(dateTime);
 
-    // create bot message and display 
+    // Create bot message and display 
     $('<div>')
     .addClass('bot-message')
     .text(randomReply)
