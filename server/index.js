4ca684fbcd2d7b0508a7ea1e24b554875303906b
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2024-01-31 16:41:58 +0100

    Feat: Add server with support for db and sequelize

--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
-import Koa from 'koa';
+
 import bodyParser from 'koa-bodyparser';
+import db from ('./models/index.js');
+import Koa from 'koa';
 import serve from 'koa-static';
+import router from ('./router');
 
 const app = new Koa();
 const PORT = 3000;
 
-app.use(serve(config.clientPath()));
-app.use(bodyParser());
-app.use(router.routes());
+app
+  .use(serve('../../client'))
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-app.listen(PORT, () => {
+
+async function bootstrap () {
+    await db.sync();
+    app.listen(PORT)
     console.log(`Server running at http://localhost:${PORT}`)
-})
+}
 
+bootstrap ();
 
 
