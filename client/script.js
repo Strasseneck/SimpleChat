434df95435f60653fb96d6543029b50a9deb0130
Joe Copplestone <strasseneck@fedora.fritz.box>
2024-02-04 13:18:42 +0100

    Fix: Add missing header, Json parsing

--- client/script.js
+++ client/script.js
@@ -2,52 +2,52 @@
 let botReplyTimer;
 
 // friendly bot by default
-const currentBot = 'friendlyBot';
+let currentBot = 'friendlyBot';
 
 // class for messages
 class Message {
-    constructor (author, content, timestamp) {
-        this.author = author, 
-        this.content = content,
-        this.timestamp = timestamp;
+    constructor(author, content, timestamp) {
+        this.author = author,
+            this.content = content,
+            this.timestamp = timestamp;
     }
 }
 
 // Friendly Bot clicked
-$('#friendly-bot').on('click', function() {
+$('#friendly-bot').on('click', function () {
     currentBot = 'friendlyBot';
 
     // Reset the text area to placeholder and refocus
     $('#chat-input').val('').blur().focus();
 
     // clear out reply timer to avoid wrong bot reply
-    if(botReplyTimer !== undefined) {
+    if (botReplyTimer !== undefined) {
         clearTimeout(botReplyTimer);
     }
 
     // Clear out previous bot 
     $('.message').remove();
-    
+
     // Loop through chat history extract and create message
     let history = chatHistory.friendlyBotHistory;
     history.forEach((message) => {
         addMessage(message);
     });
-            
+
     // Change header name and icon
     $('#header-icon').attr('src', 'static/android.png');
     $('#header-name').text('Friendly Bot');
 })
 
 // Unfriendly Bot clicked
-$('#unfriendly-bot').on('click', function() {
+$('#unfriendly-bot').on('click', function () {
     currentBot = 'unfriendlyBot';
 
     // Reset the text area to placeholder and refocus
     $('#chat-input').val('').blur().focus();
 
     // clear out reply timer to avoid wrong bot reply
-    if(botReplyTimer !== undefined) {
+    if (botReplyTimer !== undefined) {
         clearTimeout(botReplyTimer);
     }
 
@@ -66,17 +66,17 @@ $('#unfriendly-bot').on('click', function() {
 })
 
 // Annoying Kid Bot clicked
-$('#kid-bot').on('click', function() {
+$('#kid-bot').on('click', function () {
     currentBot = 'annoyingKid';
 
     // Reset the text area to placeholder and refocus
     $('#chat-input').val('').blur().focus();
-    
+
     // clear out reply timer to avoid wrong bot reply
-    if(botReplyTimer !== undefined) {
+    if (botReplyTimer !== undefined) {
         clearTimeout(botReplyTimer);
     }
-    
+
     // Clear out previous bot 
     $('.message').remove();
 
@@ -94,22 +94,25 @@ $('#kid-bot').on('click', function() {
 // Messaging
 
 // Send new message on enter
-$('#chat-input').on('keypress', function(event) {
+$('#chat-input').on('keypress', (event) => sendMessage(event));
+
+// Send message function
+function sendMessage(event) {
     // Check it was enter key clicked
-    if(event.keyCode === 13) {
+    if (event.keyCode === 13) {
         // Get user message 
         let userInput = $('#chat-input').val();
-        
+
         // Check it's not empty
-        if(userInput === '') {
+        if (userInput === '') {
             return;
         }
-                
+
         //Check if the bot is replying and if so clear timer so it's not one reply per msg
-        if(botReplyTimer !== undefined) {
+        if (botReplyTimer !== undefined) {
             clearTimeout(botReplyTimer);
         }
-        
+
         let dateTime = getDatetime();
 
         const newMessage = new Message('user', userInput, dateTime);
@@ -129,31 +132,31 @@ $('#chat-input').on('keypress', function(event) {
             const newBotMessage = new Message(currentBot, reply, dateTime);
             displayMessage(newBotMessage);
             scrollDown();
-        },1500)
+        }, 1500)
     }
-})
+}
 
-function getDatetime () {
+function getDatetime() {
     // Get current date and time for tag
     let now = new Date();
-    let date = now.getDate() + '/' + (now.getMonth() +1) + '/' + now.getFullYear();
+    let date = now.getDate() + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
     let hours = now.getHours();
     let mins = now.getMinutes();
-    if(mins <= 9) {
+    if (mins <= 9) {
         mins = '0' + mins;
     }
     let time = hours + ':' + mins;
     return date + ' ' + time;
 }
 
-async function getBotReply (bot) {
+async function getBotReply(bot) {
     try {
         const response = await fetch('/botreply', {
             method: 'GET',
-            headers : { bot: bot }
+            headers: { bot: bot }
         });
         if (!response.ok) {
-            throw new Error (`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         const reply = await response.json();
         return reply;
@@ -166,17 +169,17 @@ async function getBotReply (bot) {
 function scrollDown() {
     console.log('scrollDown');
     let $chatsinner = $('#chats');
-    $chatsinner.scrollTop($chatsinner[0].scrollHeight);  
+    $chatsinner.scrollTop($chatsinner[0].scrollHeight);
 }
 
 // Add message function for populating chat history
-function displayMessage(message) {
+async function displayMessage(message) {
 
     const type = message.author === 'user' ? 'user' : 'bot';
     let $tag = $('<div>')
         .addClass('tag')
-        .text(`${message.timestamp}`);       
-    
+        .text(`${message.timestamp}`);
+
     let $newMess = $('<div>')
         .addClass('message')
         .addClass(`message-${type}`);
@@ -184,16 +187,16 @@ function displayMessage(message) {
     let $p = $('<p>')
         .text(`${message.content}`)
         .append($tag)
-    
-        // User icon
+
+    // User icon
     let $iconContainer = $('<div>')
         .addClass('chat-icon-container');
-    
+
     let $icon = $('<img>')
         .addClass('chat-icon')
-        .attr('src', `static/${message.user}.png`);
-    
-    
+        .attr('src', `./static/${message.author}.png`);
+
+
     $iconContainer.append($icon);
     $newMess.append($p)
     $newMess.append($iconContainer)
@@ -201,10 +204,64 @@ function displayMessage(message) {
 
     // Scroll down
     scrollDown();
-};
-            
 
-       
-       
+   try {
+     saveMessage (message);
+   }
+   catch (error) {
+    console.error (error);
+   }
+};
+
+// Save message function
+async function saveMessage(message) {
+    try {
+        const response = await fetch('/messages', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const res = await response.json();
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Get chat history
+async function getChatHistory(bot) {
+    let chatHistory;
+    try {
+        const response = await fetch('/getMessages', {
+            method: 'GET',
+            headers: { bot: bot }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        chatHistory = await response.json();
+    }
+    catch (error) {
+        console.error(error);
+    }
+    finally {
+        console.log(`chat history retrieved: ${chatHistory}`);
+        // Loop through chat history extract and create message
+        chatHistory.forEach((message) => {
+            displayMessageMessage(message);
+        });
+    }
+
+}
+
+
+
+
 
 
