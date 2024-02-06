c4318c516e2aa8531c8198548ad42aabf5f528c9
Joe Copplestone <strasseneck@gmail.com>
2024-02-06 09:25:28 +0100

    Fix: Add better logging on Sequelize instance

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ import {  password } from './password.js'
 const sequelize = new Sequelize ('simplechat', 'strasseneck', password, {
     host: '127.0.0.1',
     dialect: 'postgres',
+    logging: (...msg) => console.log(msg)
 })
 
 export { sequelize };
