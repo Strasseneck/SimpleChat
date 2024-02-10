1a4e3c21f569189c982bcb2b77aaee29483336a0
Joe Copplestone <strasseneck@gmail.com>
2024-02-10 17:22:39 +0100

    Feat: Add messages being displayed as plain text

--- client/client/src/app/api-client.service.ts
+++ client/client/src/app/api-client.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Message } from './message';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiClientService {
   
-  private baseUrl = 'http://localhoste:3000';
+  private baseUrl = 'http://localhost:3000';
+  
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
   
@@ -17,31 +19,42 @@ export class ApiClientService {
       console.error(error); // log to console instead
   
       // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      console.log(`${operation} failed: ${error.message}`);
   
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
   
-
   constructor(public http: HttpClient) { }
 
 
   // get chat history for a bot
   getChatHistory(bot: string): Observable<Message[]> {
+    const url = `${this.baseUrl}/messages/${bot}`;
     const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type':  'application/json', 'Chat': `${bot}`})
+      headers: new HttpHeaders()
+      .set('Content-Type','application/json')
     };
-    return this.http.get <Message[]> (`${this.baseUrl}/messages`);
+    return this.http.get <Message[]> (url);
   }
 
   // save a new message
   saveMessage(message: Message): Observable<Message> {
-    return this.http.post<Message>(`${this.baseUrl}/messages`, message)
+    const url = `${this.baseUrl}/messages`
+    return this.http.post<Message>(url, message)
       .pipe(
         catchError(this.handleError('saveMessage', message))
       )
+  }
+
+  // get a random bot reply
+   getBotReply(bot: string): Observable<Message[]> {
+    const url = `${this.baseUrl}/botReply`
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type':  'application/json', 'Bot': `${bot}`})
+    };
+    return this.http.get <Message[]> (url)
   }
 
 }
--- client/client/src/app/chat-container/chat-container.component.ts
+++ client/client/src/app/chat-container/chat-container.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core'
+import { ApiClientService } from '../api-client.service';
+import { Message } from '../message';
 @Component({
   selector: 'app-chat-container',
   templateUrl: './chat-container.component.html',
   styleUrl: './chat-container.component.css'
 })
-export class ChatContainerComponent {
+export class ChatContainerComponent implements OnInit{
+  
+  chatHistory: Message[] = [];
+  currentBot: string = 'friendlyBot';
 
+  constructor(private apiClientServices: ApiClientService) {}
+
+  ngOnInit(): void {
+    console.log('TRYING TO GET CHAT HISTORY')
+    // get friendlyBot messages
+    this.apiClientServices.getChatHistory(this.currentBot)
+      .subscribe(response => {
+        this.chatHistory = response;
+        console.log(this.chatHistory)
+      })
+  }
 }
